469fd1fd1113633fc7ab714ccf53e9e110aff13a
caeklol <me@caek.dev>
2025-02-02 20:48:56 +0800

    create theme event

--- srv/index.js
+++ srv/index.js
@@ -1,14 +1,28 @@
-window.onload = () => {
-	const checkbox = document.getElementsByTagName('input')[0];
+function sync(t) {
 	const elements = document.querySelectorAll('*');
+	if (t) {
+		elements.forEach(e => e.classList.add('dark'));
+	} else {
+		elements.forEach(e => e.classList.remove('dark'));
+	}
 
-	checkbox.addEventListener('change', () => {
-		if (checkbox.checked) {
-			elements.forEach(e => e.classList.add('dark'));
-		} else {
-			elements.forEach(e => e.classList.remove('dark'));
-		}
-	});
+	window.theme = t;
+	localStorage.setItem("theme", t);
+}
 
-	elements.forEach(e => e.classList.add('dark'));
-};
+document.addEventListener("theme", (e) => sync(e.detail));
+
+document.addEventListener("DOMContentLoaded", () => {
+	const toggle = document.getElementById("toggle");
+	let triggerEvent = () => {
+		document.dispatchEvent(new CustomEvent("theme", {
+			detail: toggle.checked
+		}));
+	};
+
+	toggle.checked = localStorage.getItem("theme") == 'true' ?? true;
+
+	toggle.addEventListener('change', triggerEvent);
+
+	triggerEvent();
+});
