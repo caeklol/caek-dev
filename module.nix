8e7744530fa92157534a4e7d2df4c4e6d157a072
caeklol <me@caek.dev>
2025-02-02 21:01:15 +0800

    nix fmt & matrix synapse

--- module.nix
+++ module.nix
@@ -1,18 +1,31 @@
 self: {
-   config,
-   pkgs,
-   lib,
-   ...
+  config,
+  pkgs,
+  lib,
+  ...
 }: let
-   srv = builtins.path {
-	name = "caek-dev-src";
-	path = ./srv;
-   };
+  srv = builtins.path {
+    name = "caek-dev-srv";
+    path = ./srv;
+  };
 in {
-   services.caddy.virtualHosts."caek.dev" = {
-	extraConfig = ''
-		root * ${srv}
-		file_server
-	'';
-   };
+  services.matrix-synapse = {
+    enable = true;
+  };
+
+  services.caddy.virtualHosts = {
+    "caek.dev".extraConfig = ''
+      root * ${srv}
+      file_server
+    '';
+
+    "matrix.caek.dev".extraConfig = ''
+      reverse_proxy /_matrix/* localhost:8008
+      		reverse_proxy /_synapse/client/* localhost:8008
+    '';
+
+    "example.com:8448".extraConfig = ''
+      reverse_proxy /_matrix/* localhost:8008
+    '';
+  };
 }
