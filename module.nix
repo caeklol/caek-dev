c5cc7225810fb073a9713c852d94c597dd6c67d1
caeklol <me@caek.dev>
2025-03-16 03:13:39 +0800

    give up on matrix for now

--- module.nix
+++ module.nix
@@ -9,23 +9,10 @@ self: {
     path = ./srv;
   };
 in {
-  services.matrix-synapse = {
-    enable = true;
-  };
-
   services.caddy.virtualHosts = {
     "caek.dev".extraConfig = ''
       root * ${srv}
       file_server
-    '';
-
-    "matrix.caek.dev".extraConfig = ''
-      reverse_proxy /_matrix/* localhost:8008
-      		reverse_proxy /_synapse/client/* localhost:8008
-    '';
-
-    "example.com:8448".extraConfig = ''
-      reverse_proxy /_matrix/* localhost:8008
     '';
   };
 }
